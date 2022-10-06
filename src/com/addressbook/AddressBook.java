2c21a9e605185e20f7b9cbe59e0cd83b53ef84d9
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-10-06 21:41:39 +0530

    [Dipali] Added : add information

---
+++ src/com/addressbook/AddressBook.java
@@ -0,0 +1,14 @@
+package com.addressbook;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+	ArrayList<Contact> contacts = new ArrayList<>();
+
+	public void createContact(String firstName, String lastName, String street, String city, String state, int zip,
+			long phoneNo, String email) {
+		Contact contact = new Contact(firstName, lastName, street, city, state, zip, phoneNo, email);
+		contacts.add(contact);
+	}
+
+}
---
+++ src/com/addressbook/AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.addressbook;
+
+import java.util.ArrayList;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+
+		AddressBook addressBook = new AddressBook();
+
+	}
+}
